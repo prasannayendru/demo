f3b2b705aa6a0d295faf079b66c56127ac7e30c9
prasannayendru <109511992+prasannayendru@users.noreply.github.com>
2022-08-22 14:49:35 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.helloworld;
+
+/**
+ *
+ * @author ITLab1-20
+ */
+public class HelloWorld {
+    public static void main(String args[]){
+        System.out.println("Hello World");
+    }
+    
+}
